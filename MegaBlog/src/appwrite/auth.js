42caca07bf0e24c712109b6504d5ea8f51115758
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-01-12 11:56:39 -0200

    Authentication

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,10 @@
+import conf from '../conf.js'
+import {Clinet,Account,ID} from 'appwrite'
+export class Authentication {
+    Clinet = new Clinet();
+    Account;
+
+    constructor(){
+        this.Clinet
+    }
+}
--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,10 +1,10 @@
 const conf = {
 
-     appwriteurl: String(import.meta.env.VITE_APPWRITE_URL),
-     appwriteprojectid: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-     appwritedatabaseid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-     appwritecollectionid: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-     appwritebucketid: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+     appwriteSDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 
 }
 
