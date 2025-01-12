4a7cd538784889bac83dbdc2ae725bf02b16eeda
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-01-12 12:05:42 -0200

    AuthService

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -1,10 +1,24 @@
 import conf from '../conf.js'
 import {Clinet,Account,ID} from 'appwrite'
-export class Authentication {
+
+
+export class AuthService {
     Clinet = new Clinet();
-    Account;
+    account;
 
     constructor(){
         this.Clinet
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account=new Account(this.Clinet);
     }
 }
+
+const authService =  new AuthService();
+
+export default authService;
+
+
+// now We Createt  Account 
+// 10:42
